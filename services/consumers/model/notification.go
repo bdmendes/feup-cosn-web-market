1ef19a91d1cf87a3ebb86701e72fbabc6c168839
José Costa <sirze@live.com.pt>
2023-12-14 01:07:06 +0000

    Fix notifications model

--- services/consumers/model/notification.go
+++ services/consumers/model/notification.go
@@ -1,8 +1,8 @@
 package model
 
 type PriceDropNotification struct {
-	ConsumerID string  `bson:"consumerId" json:"consumerId"`
-	ProductID  string  `bson:"productId" json:"productId"`
-	OldPrice   float32 `bson:"oldPrice,truncate" json:"oldPrice"`
-	NewPrice   float32 `bson:"newPrice,truncate" json:"newPrice"`
+	ConsumerID string      `bson:"consumerId" json:"consumerId"`
+	ProductID  interface{} `bson:"productId" json:"productId"`
+	OldPrice   float32     `bson:"oldPrice,truncate" json:"oldPrice"`
+	NewPrice   float32     `bson:"newPrice,truncate" json:"newPrice"`
 }
